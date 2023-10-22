bd143d1a0491153c6ca2a9d8355c4384aaeedc7d
Maxim Solodilov <sldmxm@gmail.com>
2023-10-22 10:53:01 +0300

    1st merge

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,18 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from .views import StudentViewSet
-
-app_name = 'api'
-
-router = DefaultRouter()
-
-router.register('students', StudentViewSet, basename='students')
-
-urlpatterns = [
-    path('v1/', include(router.urls)),
-]
+urlpatterns = (
+    path(
+        'v1/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'v1/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+)
