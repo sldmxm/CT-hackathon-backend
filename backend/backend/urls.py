98677855a3d85151a8f6893bc862a87bb591dd17
Maxim Solodilov <sldmxm@gmail.com>
2023-10-26 11:38:02 +0300

    добавление админа

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,35 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-urlpatterns = (
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
+
+
+urlpatterns = [
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path('api/', include('api.urls', namespace='api')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
