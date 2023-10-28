98c71d05078c80ed425a978418504ef4e4bdf50b
Maxim Solodilov <sldmxm@gmail.com>
2023-10-29 01:14:03 +0300

    feature: доработана команда filldb, добавляет админа и ему вакансии

--- backend/utils/management/commands/filldb.py
+++ backend/utils/management/commands/filldb.py
@@ -4,26 +4,37 @@ import os
 from django.apps import apps
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-from utils import constants
+from utils.constants import (
+    CANDIDATES_COUNT,
+    DATA_DIR,
+    STUDENTS_COUNT,
+    USERS_COUNT,
+    VACANCIES_COUNT,
+    VACANCY_ADMIN_AUTHOR,
+)
 from utils.factories import (
     StudentFactory,
     UserFactory,
     VacancyFactory,
-    VacancyStudentsFactory,
+    VacancyStudentFactory,
 )
+from vacancies.models import Vacancy
+
+from .superuser import Command as SuperuserCommand
 
 
 class Command(BaseCommand):
     help = '''
-    Чистит БД, добавляет данные из data, генерирует остальные
+    Чистит БД, добавляет данные из data, генерирует остальные,
+    создает админа из .env, добавляет в вакансии его автором.
     '''
 
     def handle(self, *args, **options):
         def import_data_from_csv():
             models = apps.get_models()
 
-            for filename in os.listdir(constants.DATA_DIR):
-                file_path = os.path.join(constants.DATA_DIR, filename)
+            for filename in os.listdir(DATA_DIR):
+                file_path = os.path.join(DATA_DIR, filename)
                 model_name = os.path.splitext(filename)[0]
 
                 model_found = False
@@ -48,16 +59,41 @@ class Command(BaseCommand):
                 if count > 0:
                     model.objects.bulk_create(data)
                     print(
-                        f"Добавлено {count} записей в модель '{model_name}'.")
+                        f"Добавлено {count}"
+                        f" записей в модель '{model_name}'.")
                 else:
                     print(
-                        f"Файл '{filename}' не содержит данных для импорта.")
+                        f"Файл '{filename}'"
+                        f" не содержит данных для импорта.")
+
+        def vacancy_author_update(new_author, vacancies_count):
+            vacancies_to_update = (
+                Vacancy.objects.order_by('?')[:vacancies_count])
+            for vacancy in vacancies_to_update:
+                vacancy.admin_author = new_author
+            Vacancy.objects.bulk_update(vacancies_to_update, ['author'])
 
         call_command('flush', interactive=False)
 
+        admin_user = SuperuserCommand().handle()
+
         import_data_from_csv()
 
-        StudentFactory.create_batch(constants.STUDENTS_COUNT)
-        UserFactory.create_batch(constants.USERS_COUNT)
-        VacancyFactory.create_batch(constants.VACANCIES_COUNT)
-        VacancyStudentsFactory.create_batch(constants.CANDIDATES_COUNT)
+        StudentFactory.create_batch(STUDENTS_COUNT)
+        print(
+            f"Добавлено {STUDENTS_COUNT}"
+            f" записей в модель 'Student'.")
+        UserFactory.create_batch(USERS_COUNT)
+        print(
+            f"Добавлено {USERS_COUNT}"
+            f" записей в модель 'User'.")
+        VacancyFactory.create_batch(VACANCIES_COUNT)
+        print(
+            f"Добавлено {VACANCIES_COUNT}"
+            f" записей в модель 'Vacancy'.")
+        VacancyStudentFactory.create_batch(CANDIDATES_COUNT)
+        print(
+            f"Добавлено {CANDIDATES_COUNT} "
+            f"записей в модель 'VacancyStudent'.")
+
+        vacancy_author_update(admin_user, VACANCY_ADMIN_AUTHOR)
