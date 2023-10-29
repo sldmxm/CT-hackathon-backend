c9bd3a591582d2b66a8e30ef83dc37aa234d6d69
Maxim Solodilov <sldmxm@gmail.com>
2023-10-29 20:50:03 +0200

    CORS_ALLOW_ALL_ORIGINS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,20 +138,22 @@ AUTH_USER_MODEL = 'users.User'
 
 # CORS
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
-
-CORS_ALLOW_HEADERS = [
-    'Accept',
-    'Authorization',
-    'Content-Type',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+#
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
+# CORS_ALLOW_METHODS = [
+#     'DELETE',
+#     'GET',
+#     'OPTIONS',
+#     'PATCH',
+#     'POST',
+#     'PUT',
+# ]
+#
+# CORS_ALLOW_HEADERS = [
+#     'Accept',
+#     'Authorization',
+#     'Content-Type',
+# ]
