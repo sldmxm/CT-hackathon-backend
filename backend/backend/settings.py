003aaddbb91c4f257e9208caecf4938fa7737845
Maxim Solodilov <sldmxm@gmail.com>
2023-10-31 09:01:33 +0200

    fix cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,9 +137,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'users.User'
 
 # CORS
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = ["http://130.193.36.223"]
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_CREDENTIALS = True
 #
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:5173",
